e097442ca63aaa608f1d8fd2d5a03c0a80f34d1b
Siddharth Rawat <rawat.sid@northeastern.edu>
2024-01-12 14:48:08 -0500

    fix: update build script and healthz endpoint

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -4,6 +4,6 @@ import { health } from '../controllers/health.controller.js'
 const router = express.Router()
 
 router.get('/', health)
-router.get('/health', health)
+router.get('/healthz', health)
 
 export { router as healthRoute }
